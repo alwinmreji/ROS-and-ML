8e8ef1a5cc0c1560889ddb8982e65d553914b2db
Alwin M Reji <alwinreji18@gmail.com>
2019-08-27 11:37:42 +0530

    Create #8_largest_among_three.py

---
+++ Assingment_#1/#8_largest_among_three.py
@@ -0,0 +1,10 @@
+######################################################
+#Find largest among three
+
+print("Largest value ",max(int(input("Enter the first number: ")),int(input("Enter the second number: ")),int(input("Enter the third number: "))))
+
+# OUPUT:
+# Enter the first number: 1
+# Enter the second number: 6
+# Enter the third number: 4
+# Largest value  6
