ce3e3a4d40152db0e435be9e225082629e542882
Alwin M Reji <alwinreji18@gmail.com>
2019-08-27 11:18:14 +0530

    Create #3_palindrome.py

---
+++ #3_palindrome.py
@@ -0,0 +1,9 @@
+##########################################################
+#String Palindrome checker
+
+a = input("Enter a string:\t")
+print(("Is a Palindrome")if a==a[::-1] else("Not a Palindrome"))
+
+#OUTPUT:
+# Enter a string:	dad
+# Is a Palindrome
