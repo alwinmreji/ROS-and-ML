089ff4df5fc5208f061b8625d125f1ce090acf69
Alwin M Reji <alwinreji18@gmail.com>
2019-09-27 23:37:00 +0530

    Create #1_max_outof_two.py

---
+++ Assingment_#2/#1_max_outof_two.py
@@ -0,0 +1,8 @@
+def maximum(x,y):
+  if x>y:
+    return x
+  else:
+    return y
+x = float(input("Enter first variable:\t"))
+y = float(input("Enter second variable:\t"))
+print("Greatest is ",maximum(x,y))
