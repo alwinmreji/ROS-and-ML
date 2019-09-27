f494e0271b6d13252fb26f4f499eb7591daa927a
Alwin M Reji <alwinreji18@gmail.com>
2019-09-27 23:38:43 +0530

    Update #1_max_outof_two.py

--- Assingment_#2/#1_max_outof_two.py
+++ Assingment_#2/#1_max_outof_two.py
@@ -6,3 +6,7 @@ def maximum(x,y):
 x = float(input("Enter first variable:\t"))
 y = float(input("Enter second variable:\t"))
 print("Greatest is ",maximum(x,y))
+# OUTPUT
+# Enter first variable:   4
+# Enter second variable:  5
+# Greatest is  5.0
