2f8ea90bc06ad437712f52f855837b17b9dbc62f
Alwin M Reji <alwinreji18@gmail.com>
2019-09-30 23:23:17 +0530

    Update #17_max_1_min_0.py

--- Assingment_#2/#17_max_1_min_0.py
+++ Assingment_#2/#17_max_1_min_0.py
@@ -12,3 +12,13 @@ print(mini)
 maxi = numpy.amax(smpl, 0) 
 print("Printing amax Of Axis 0")
 print(maxi)
+
+# OUTPUT
+# Printing Original array
+# [[34 43 73]
+#  [82 22 12]
+#  [53 94 66]]
+# Printing amin Of Axis 1
+# [34 12 53]
+# Printing amax Of Axis 0
+# [82 94 73]
