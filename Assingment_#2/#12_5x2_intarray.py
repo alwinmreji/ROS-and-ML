473a5e919c864083aa17cec911c58b0d246ca751
Alwin M Reji <alwinreji18@gmail.com>
2019-09-30 22:41:27 +0530

    Update #12_5x2_intarray.py

--- Assingment_#2/#12_5x2_intarray.py
+++ Assingment_#2/#12_5x2_intarray.py
@@ -4,3 +4,10 @@ print("Creating 5X2 array using np.arange")
 smpl = np.arange(100, 200, 10)
 smpl = smpl.reshape(5,2)
 print (smpl)
+# OUTPUT
+# Creating 5X2 array using np.arange
+# [[100 110]
+#  [120 130]
+#  [140 150]
+#  [160 170]
+#  [180 190]]
