f02b3803e8839d41ce8a9d72f4c65b318ca8c5ff
Alwin M Reji <alwinreji18@gmail.com>
2019-09-30 22:39:25 +0530

    Update #11_4x2_intarray.py

--- Assingment_#2/#11_4x2_intarray.py
+++ Assingment_#2/#11_4x2_intarray.py
@@ -8,3 +8,13 @@ print("Printing np array Attributes")
 print("1> Array Shape is: ", first.shape)
 print("2>. Array dimensions are ", first.ndim)
 print("3>. Length of each element of array in bytes is ", first.itemsize)
+#OUTPUT
+# Printing Array
+# [[64757 19267]
+#  [45868 33230]
+#  [16280 54129]
+#  [32584     0]]
+# Printing numpy array Attributes
+# 1> Array Shape is:  (4, 2)
+# 2>. Array dimensions are  2
+# 3>. Length of each element of array in bytes is  2
